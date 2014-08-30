fb1ac7c735afa4759e90679bf4eef720e6c5fae4
Chanin N <chaninz@live.com>
2014-08-31 02:27:56 +0700

    Reorganize /images /assets and create header.php, footer.php for assets including

---
+++ application/views/footer.php
@@ -0,0 +1,13 @@
+<script>
+		$(document)
+			.ready(function() {
+				$('.ui.dropdown')
+				.dropdown()
+				;
+				$('.ui.checkbox')
+				.checkbox()
+				;
+		})
+		;
+	</script>
+	<script src="assets/js/bandbrary.js"></script>
---
+++ application/views/header.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/semantic.min.css">
+	<link rel="stylesheet" href="assets/css/bg-slide.css">
+	<link rel="stylesheet" href="assets/css/bandbrary.css">
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.address.min.js"></script>
+	<script src="assets/js/semantic.js"></script>
+	<script src="assets/js/bg-slide.js"></script>
