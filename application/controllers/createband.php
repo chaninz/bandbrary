1b5ef810e998cce986930b30c5a95e90c12204ea
PUNPUNsa <sa.punpun@gmail.com>
2014-09-04 22:44:29 +0700

    Update create job

--- application/controllers/createband.php
+++ application/controllers/createband.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class CreateBand extends CI_Controller {
+class Createjob extends CI_Controller {
 	public function index(){
 		$this->load->view('createband');		
 	}
