a26a6406270d2b08eb880a68dd769586320af667
Chetniphat Varasai <titrez@outlook.com>
2014-09-10 02:35:36 +0700

    Reindent

--- application/views/navigation.php
+++ application/views/navigation.php
@@ -1,58 +1,58 @@
 <header>
-		<div class="navbar-item1">	
-			<i class="home icon" style="font-size: 2.7rem"></i>
-			<div class="ui basic buttons">
-				<div class="ui button">Explore</div>
-				<div class="ui button">Jobs</div>
-			</div>
+	<div class="navbar-item1">	
+		<i class="home icon" style="font-size: 2.7rem"></i>
+		<div class="ui basic buttons">
+			<div class="ui button">Explore</div>
+			<div class="ui button">Jobs</div>
 		</div>
-		<div class="navbar-item2">
-			<div class="ui icon input">
-				<input type="text" placeholder="Search...">
-				<i class="search icon" style="font-size: 1.2rem"></i>
-			</div>
+	</div>
+	<div class="navbar-item2">
+		<div class="ui icon input">
+			<input type="text" placeholder="Search...">
+			<i class="search icon" style="font-size: 1.2rem"></i>
 		</div>
-		<div class="bb-logo">
-			<img src="../../images/bangbrary_logo_16-9.png">
-		</div>
-		<div class="navbar-item3">
-			<div class="ui compact menu">
-				<div class="ui pointing dropdown link item">
-					<img src="" alt="" class="profile-image">
-					<div class="user-name">
-						Bandbrary
-					</div>
-					<i class="dropdown icon" style="font-size: 1.2rem;"></i>
-					<div class="menu">
-						<a class="item"></i>Music</a>
-						<a class="item"></i>Post</a>
-						<a class="item"></i>Follower</a>
-						<a class="item"></i>Following</a>
-						<a class="item"></i>Event</a>
-					</div>
+	</div>
+	<div class="bb-logo">
+		<img src="../../images/bangbrary_logo_16-9.png">
+	</div>
+	<div class="navbar-item3">
+		<div class="ui compact menu">
+			<div class="ui pointing dropdown link item">
+				<img src="" alt="" class="profile-image">
+				<div class="user-name">
+					Bandbrary
+				</div>
+				<i class="dropdown icon" style="font-size: 1.2rem;"></i>
+				<div class="menu">
+					<a class="item"></i>Music</a>
+					<a class="item"></i>Post</a>
+					<a class="item"></i>Follower</a>
+					<a class="item"></i>Following</a>
+					<a class="item"></i>Event</a>
 				</div>
 			</div>
 		</div>
-		<div class="navbar-item4">
-			<i class="bell icon"></i>
-			<i class="mail icon"></i>
-			<div class="ui pointing dropdown icon">
-				<i class="settings icon"></i>
-				<div class="menu" style="margin-top: 1.11em; border-top:0px;">
-					<div class="item">Go to Band</div>
-					<div class="line"></div>
-					<div class="item">Create Band</div>
-					<div class="item">Manage Band</div>
-					<div class="line"></div>
-					<div class="item">Create Job</div>
-					<div class="item">Manage Job</div>
-					<div class="line"></div>
-					<div class="item">Setting</div>
-					<div class="item">Sign out</div>
-					<div class="line"></div>
-					<div class="item">Help</div>
-					<div class="item">Report a Problem</div>
-				</div>
+	</div>
+	<div class="navbar-item4">
+		<i class="bell icon"></i>
+		<i class="mail icon"></i>
+		<div class="ui pointing dropdown icon">
+			<i class="settings icon"></i>
+			<div class="menu" style="margin-top: 1.11em; border-top:0px;">
+				<div class="item">Go to Band</div>
+				<div class="line"></div>
+				<div class="item">Create Band</div>
+				<div class="item">Manage Band</div>
+				<div class="line"></div>
+				<div class="item">Create Job</div>
+				<div class="item">Manage Job</div>
+				<div class="line"></div>
+				<div class="item">Setting</div>
+				<div class="item">Sign out</div>
+				<div class="line"></div>
+				<div class="item">Help</div>
+				<div class="item">Report a Problem</div>
 			</div>
 		</div>
-	</header>
+	</div>
+</header>
