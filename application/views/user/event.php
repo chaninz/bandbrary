503ae031787f2c36910d85b68e501e6d9df5c791
Chetniphat Varasai <titrez@outlook.com>
2014-09-29 02:19:06 +0700

    Create view add event

--- application/views/user/event.php
+++ application/views/user/event.php
@@ -21,6 +21,11 @@
 		-webkit-box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);
 		box-shadow: 0 1px 1px rgba(0,0,0,.24),0 1px 5px rgba(0,0,0,.05);
 	}
+	#event-add-btn {
+		margin-left: 505px;
+		margin-bottom: 15px;
+		font-size: 1.1rem;
+	}
 	</style>
 </head>
 <body>
@@ -33,6 +38,10 @@
 					<?php $this->load->view('user/sidebar_left'); ?>
 					<div class="col-xs-7">
 						<div class="center">
+							<a id="event-add-btn" class="ui red button test event">
+								<i class="edit icon"></i>
+								Add event
+							</a>
 							<div class="ui fluid accordion">
 								<div class="event-hea">
 									<table>
@@ -66,6 +75,61 @@
 		</article>
 	</section>
 
-	<?php $this->load->view('footer'); ?>
-</body>
-</html>
+	<!--Add event modal-->
+	<div class="ui form segment create modal">
+		<i class="close icon"></i>
+		<form action="<?php echo base_url().'band/post/add'; ?>" method="post">
+			<h3>Add event</h3>
+			<div class="line"></div>
+			<p/>
+			<div class="field">
+				<label>Title</label>
+				<input type="text" placeholder="" name="topic" required>
+			</div>
+			<div class="field">
+				<label>Province</label>
+				<div class="ui fluid selection dropdown">
+					<div class="text">Select</div>
+					<i class="dropdown icon"></i>
+					<input type="hidden" name="province">
+					<div class="menu">
+						<div class="item" data-value="1" style="font-size: 14px;">Bangkok</div>
+						<div class="item" data-value="2" style="font-size: 14px;">Changmai</div>
+					</div>
+				</div>
+			</div>
+			<div class="line"></div>
+			<p/>
+			<div class="field">
+				<label>Description</label>
+				<textarea name="post"></textarea>
+			</div>
+			<div class="two fields">
+				<div class="field">
+					<label>Date</label>
+					<div class="ui left labeled icon input">
+						<input type="date" placeholder="" style="padding: .2em 1em;" name="start_time">
+						<i class="calendar icon"></i>
+					</div>
+				</div>
+				<div class="field">
+					<label>Time</label>
+					<input type="time" placeholder="" style="padding: .2em 1em;" name="time" required>
+				</div>
+			</div>
+					<div class="line"></div>
+					<p/>
+					<div class="actions">
+						<div class="ui button">cancel</div>
+						<input type="submit" class="ui red submit button" value="Add event">
+					</div>
+				</form>
+			</div>
+
+			<?php $this->load->view('footer'); ?>
+			<script>
+			$('.create.modal')
+			.modal('attach events', '.test.event', 'show');
+			</script>
+		</body>
+		</html>
