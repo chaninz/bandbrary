9ba51262c7e34ad24ea2de94a53d29e24c2d21ce
PUNPUNsa <sa.punpun@gmail.com>
2014-09-29 16:14:33 +0700

    Update Request/role/edit

---
+++ application/controllers/band/request.php
@@ -0,0 +1,32 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Request extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('band_model');
+	}
+
+	public function index() {
+		if ($this->input->post()) {
+			$band = array('name' => $this->input->post('name'),
+				'biography' => $this->input->post('biography'),
+				'style' => $this->input->post('style'),
+				'fb_url' => $this->input->post('fburl'),
+				'tw_url' => $this->input->post('twurl'),
+				'yt_url' => $this->input->post('yturl'));
+			if ($this->band_model->is_exist($band['name']) == 0) {
+				$this->band_model->create($band);
+				echo 'success';
+			} else {
+				echo 'cannot create band';
+			}
+		} else {
+			$this->load->view('band/create');
+		}
+	}
+
+}
+
+/* End of file create.php */
+/* Location: ./application/controllers/band/create.php */
