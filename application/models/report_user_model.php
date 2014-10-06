f5cf9f8c4e28e1603ad844f892b35d24ba900224
PUNPUNsa <sa.punpun@gmail.com>
2014-10-06 18:51:13 +0700

    Update report band/music/user/post

--- application/models/report_user_model.php
+++ application/models/report_user_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Report_model extends CI_Model {
+class Report_User_model extends CI_Model {
 
 	function __construct(){
 		parent::__construct();	
@@ -8,18 +8,18 @@ class Report_model extends CI_Model {
 
 	function approve($report_id){
 		$this->db->where('id',$report_id);
-		$this->db->update('Report_Music',array('status' => 2));
+		$this->db->update('Report_User',array('status' => 2));
 		// 2 is approve	
 	}
 	function decline($report_id){
 		$this->db->where('id',$report_id);
-		$this->db->update('Report_Music',array('status' => 3));	
+		$this->db->update('Report_User',array('status' => 3));	
 		// 3 is cancle
 	}
 
 	function get_all_report(){
 		$this->db->select('*');
-		$this->db->from('Report');
+		$this->db->from('Report_User');
 		$this->db->where('status',1);
 		$this->db->order_by("timestamp", "desc"); 
 
@@ -27,9 +27,28 @@ class Report_model extends CI_Model {
 		return $query->result();
 	}	
 
+	function get_approved_report(){
+		$this->db->select('*');
+		$this->db->from('Report_User');
+		$this->db->where('status',2);
+		$this->db->order_by("timestamp", "desc"); 
+
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	function get_not_approve_report(){
+		$this->db->select('*');
+		$this->db->from('Report_User');
+		$this->db->where('status',1);
+		$this->db->order_by("timestamp", "desc"); 
+
+		$query = $this->db->get();
+		return $query->result();
+	}
 	function countReport(){
 		$this->db->select('COUNT (*)');
-		$this->db->from('Report');
+		$this->db->from('Report_User');
 		$this->db->where('status',1);
 		// 1 is waiting admin approve
 		// for admin check thier job
