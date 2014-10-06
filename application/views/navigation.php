da4f3036e5fa04fab878b38559c352c2bb9d43d6
Chetniphat Varasai <titrez@outlook.com>
2014-10-06 17:57:51 +0700

    Change container 1170px to 980px

--- application/views/navigation.php
+++ application/views/navigation.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-xs-12">
 				<div class="navbar-item1">	
-					<i class="home icon" style="font-size: 2.7rem"></i>
+					<i class="home icon" style="font-size: 1.8rem"></i>
 					<div class="ui basic buttons">
 						<div class="ui button">Discover</div>
 						<a class="ui button" href="<?= base_url().'job' ?>">Jobs</a>
@@ -12,15 +12,15 @@
 				<div class="navbar-item2">
 					<div class="ui icon input">
 						<input type="text" placeholder="Search...">
-						<i class="search icon" style="font-size: 1.2rem"></i>
+						<i class="search icon" style="font-size: 0.8rem"></i>
 					</div>
 				</div>	
 				<div class="bb-logo">
 					<img src="<?= base_url('images/bandbrary_logo_16-9.png') ?>">
 				</div>
-				<div class="bb-player">
+<!-- 				<div class="bb-player">
 					<img src="<?= base_url('images/header_player.png') ?>">
-				</div>
+				</div> -->
 				<div class="navbar-item3">
 					<div class="ui compact menu">
 						<div class="ui pointing dropdown link item"><?php if($this->session->userdata('photo_url')): ?>
@@ -29,7 +29,7 @@
 							<div class="user-name">
 								<?= $this->session->userdata('name'); ?>
 							</div>
-							<i class="dropdown icon" style="font-size: 1.2rem;"></i>
+							<i class="dropdown icon" style="font-size: 1em;"></i>
 							<div class="menu">
 								<a class="item" href="<?= base_url('user/'.$this->session->userdata('username').'/timeline') ?>"></i>Timeline</a>
 								<a class="item" href="<?= base_url('user/'.$this->session->userdata('username').'/music') ?>"></i>Music</a>
@@ -45,7 +45,7 @@
 					<i class="mail icon"></i>
 					<div class="ui pointing dropdown icon">
 						<i class="settings icon"></i>
-						<div class="menu" style="margin-top: 1.11em; border-top:0px;">
+						<div class="menu" id="setting-dd">
 							<div class="item"><a href="<?= base_url('user/'.$this->session->userdata('username')) ?>">Go to Profile</a></div>
 							<div class="line"></div>
 							<div class="item"><a href="<?= base_url().'account/edit' ?>">Setting</div>
