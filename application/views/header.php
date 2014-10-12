8fcf14041735a31aa09bc04a70b41239ea667d2a
Chetniphat Varasai <titrez@outlook.com>
2014-10-12 21:04:01 +0700

    Update header

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,9 @@
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url().'assets/css/bootstrap.css'; ?>">
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url().'assets/css/semantic.css'; ?>">
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url().'assets/css/bandbrary.css'; ?>">
+	<link type="text/css" rel="stylesheet" href="<?php echo base_url().'assets/css/pm.css'; ?>">
 	<script src="<?php echo base_url().'assets/js/jquery.min.js'; ?>"></script>
 	<script src="<?php echo base_url().'assets/js/jquery.address.min.js'; ?>"></script>
 	<script src="<?php echo base_url().'assets/js/semantic.js'; ?>"></script>
+	<script src="<?php echo base_url().'assets/js/pm.js'; ?>"></script>
 	<script src="<?php echo base_url().'assets/ckeditor/ckeditor.js'; ?>"></script>
