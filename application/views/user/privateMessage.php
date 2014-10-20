0d4a52239fbdf91048c5168c075c0a58c6bf97e2
Chetniphat Varasai <titrez@outlook.com>
2014-10-20 23:46:55 +0700

    Update pm

--- application/views/user/privateMessage.php
+++ application/views/user/privateMessage.php
@@ -9,67 +9,186 @@
   <!-- Site Properities -->
   <title>Feed Example - Semantic</title>
 
-    <?php $this->load->view('header'); ?>
+  <?php $this->load->view('header'); ?>
 
   <link rel="stylesheet" type="text/css" href="feed.css">
   <script src="feed.js"></script>
+
+  <style>
+  #pm-inbox {
+    height: 600px;
+    overflow-y: scroll; 
+    scrollbar-arrow-color:blue; 
+    scrollbar-face-color: #e7e7e7; 
+    scrollbar-3dlight-color: #a0a0a0; 
+    scrollbar-darkshadow-color:#888888
+  }
+  #pm-msg {
+    height: 417px;
+    overflow-y: scroll; 
+    scrollbar-arrow-color:blue; 
+    scrollbar-face-color: #e7e7e7; 
+    scrollbar-3dlight-color: #a0a0a0; 
+    scrollbar-darkshadow-color:#888888
+  }
+  .ui.segment {
+    -webkit-box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.0); 
+    box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.0); 
+    border-radius: 0px 0px 0px 0px;
+    margin: 0em; 
+  }
+  .ui.divided.list > .item {
+padding-left: 1rem;
+padding-right: 1rem;
+}
+  </style>
 </head>
 <body id="feed">
-    <?php $this->load->view('navigation'); ?>
+  <?php $this->load->view('navigation'); ?>
 
-  <div class="ui large inverted vertical sidebar menu">
-    
-    
-    <a class="item">
-      Joystiq <span class="ui label">113</span>
-    </a>
-    <div class="item">
-      <b>Archived Feeds</b>
-      <div class="menu">
-        <a class="item">
-          Engadget <span class="ui label">11</span>
-        </a>
-        <a class="item">
-          NY Times Tech Blog <span class="ui label">21</span>
-        </a>
+  <div class="container" style="border-left: 1px solid #D6D6D6; border-right: 1px solid #D6D6D6;">
+    <div class="row">
+      <div class="col-xs-12" style="height: 100px; border-bottom: 1px solid #D6D6D6;">
       </div>
     </div>
-    <a class="item">
-      <i class="bookmark icon"></i> Favorites
-    </a>
-    <div class="ui dropdown item">
-      <i class="add icon"></i> New
-      <div class="menu">
-        <a class="item"><i class="rss icon"></i> Feed</a>
-        <a class="item"><i class="tag icon"></i> Tag</a>
-        <a class="item"><i class="folder icon"></i> Group</a>
+    <div class="row">
+      <div class="col-xs-4" style="padding-top: 20px; padding-left: 0px; padding-right: 0px; border-right: 1px solid #D6D6D6;">
+        <h2 class="ui header" style="font-size: 1.3em; margin-left: 20px;">
+          <i class="inbox icon"></i>
+          กล่องข้อความ
+        </h2>
+        <div id="pm-inbox" class="ui fluid divided inbox selection list active tab" data-tab="unread">
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 14, 2013</div>
+            <div class="description">Weekly Webcomic Wrapup fought the law, and the law won</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 14, 2013</div>
+            <div class="description">Scientists discover new breed of dog</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 10, 2013</div>
+            <div class="description">Dogs colony in Antarctica</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 09, 2013</div>
+            <div class="description">Famous dog whisperer Chakotay dies today at 104</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 07, 2013</div>
+            <div class="description">Top 10 Things to Know about Labradoodles</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 05, 2013</div>
+            <div class="description">Study shows children enjoy the company of animals</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 05, 2013</div>
+            <div class="description">Study shows children enjoy the company of animals</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 05, 2013</div>
+            <div class="description">Study shows children enjoy the company of animals</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 05, 2013</div>
+            <div class="description">Study shows children enjoy the company of animals</div>
+          </a>
+          <a class="item">
+            <div class="left floated ui star rating"><i class="icon"></i></div>
+            <div class="right floated date">Sep 05, 2013</div>
+            <div class="description">Study shows children enjoy the company of animals</div>
+          </a>
+        </div>
+      </div>
+      <div class="col-xs-8" style="padding: 0px 0px 0px 0px;">
+       <div id="pm-msg" class="ui feed segment">
+        <div class="event">
+          <div class="label">
+            <img src="/images/demo/avatar.jpg">
+          </div>
+          <div class="content">
+            <div class="date">
+              3 days ago
+            </div>
+            <div class="summary">
+             <a>Sally Poodle</a> added 2 photos of you
+           </div>
+           <div class="extra images">
+            <img src="/images/demo/item1.jpg">
+            <img src="/images/demo/item2.jpg">
+          </div>
+        </div>
+      </div>
+      <div class="event">
+        <div class="label">
+          <img src="/images/demo/avatar.jpg">
+        </div>
+        <div class="content">
+          <div class="date">
+            3 days ago
+          </div>
+          <div class="summary">
+           <a>Sally Poodle</a> created a post
+         </div>
+         <div class="extra text">
+          I am a dog and I do not know how to make a post
+        </div>
+      </div>
+    </div>
+    <div class="event">
+        <div class="label">
+          <img src="/images/demo/avatar.jpg">
+        </div>
+        <div class="content">
+          <div class="date">
+            3 days ago
+          </div>
+          <div class="summary">
+           <a>Sally Poodle</a> created a post
+         </div>
+         <div class="extra text">
+          I am a dog and I do not know how to make a post
+        </div>
+      </div>
+    </div>
+    <div class="event">
+        <div class="label">
+          <img src="/images/demo/avatar.jpg">
+        </div>
+        <div class="content">
+          <div class="date">
+            3 days ago
+          </div>
+          <div class="summary">
+           <a>Sally Poodle</a> created a post
+         </div>
+         <div class="extra text">
+          I am a dog and I do not know how to make a post
+        </div>
       </div>
     </div>
   </div>
-  <div class="ui celled grid">
-    <div class="seven wide middle column">
-      <div class="ui right floated black launch button">
-        <i class="list layout icon"></i> Menu
-      </div>
-      <h2 class="ui header">
-        <i class="inbox icon"></i>
-        Inbox
-      </h2>
-      <div class="ui tabular filter menu">
-        <a class="active item" data-tab="unread">Unread</a>
-        <a class="item" data-tab="saved">Saved</a>
-        <a class="item" data-tab="all">All</a>
-      </div>
+  <textarea name="description" class="ckeditor" style"border-top: 1px solid #b6b6b6;"></textarea>
+</div>
 
+</div>
+<div class="row">
+      <div class="col-xs-12" style="height: 30px; border-top: 1px solid #D6D6D6;">
+      </div>
+    </div>
+</div>
 
-      <div class="ui divided inbox selection list active tab" data-tab="unread">
-        <a class="active item">
-          <div class="left floated ui star rating"><i class="icon"></i></div>
-          <div class="right floated date">Sep 14, 2013</div>
-          <div class="description">Weekly Webcomic Wrapup fought the law, and the law won</div>
-        </a>
-          
-          <?php foreach($pm_users as $pm_user): ?>
+   <!--   <?php foreach($pm_users as $pm_user): ?>
           <?php if($pm_user->photo_url): ?>
           <a class="item">
           <div class="left floated ui star rating"> <img src="<?= base_url().'uploads/profile/'.$pm_user->photo_url ?>"><?php else: ?>
@@ -78,70 +197,6 @@
           <div class="right floated date"><?= $pm_user->timestamp ?></div>
           <div class="description"><?= $pm_user->text ?></div>
         </a>
-              <?php endforeach; ?>
-
-        <a class="item">
-          <div class="left floated ui star rating"><i class="icon"></i></div>
-          <div class="right floated date">Sep 10, 2013</div>
-          <div class="description">Dogs colony in Antarctica</div>
-        </a>
-        <a class="item">
-          <div class="left floated ui star rating"><i class="icon"></i></div>
-          <div class="right floated date">Sep 09, 2013</div>
-          <div class="description">Famous dog whisperer Chakotay dies today at 104</div>
-        </a>
-        <a class="item">
-          <div class="left floated ui star rating"><i class="icon"></i></div>
-          <div class="right floated date">Sep 07, 2013</div>
-          <div class="description">Top 10 Things to Know about Labradoodles</div>
-        </a>
-        <a class="item">
-          <div class="left floated ui star rating"><i class="icon"></i></div>
-          <div class="right floated date">Sep 05, 2013</div>
-          <div class="description">Study shows children enjoy the company of animals</div>
-        </a>
-      </div>
-      <div class="ui divided inbox selection list tab" data-tab="saved">
-        <a class="item">
-          <div class="left floated ui star rating"><i class="icon"></i></div>
-          <div class="right floated date">Sep 14, 2013</div>
-          <div class="description">Your favorite saved article</div>
-        </a>
-        <a class="item">
-          <div class="left floated ui star rating"><i class="icon"></i></div>
-          <div class="right floated date">Sep 14, 2013</div>
-          <div class="description">Your favorite saved article</div>
-        </a>
-        <a class="item">
-          <div class="left floated ui star rating"><i class="icon"></i></div>
-          <div class="right floated date">Sep 14, 2013</div>
-          <div class="description">Your favorite saved article</div>
-        </a>
-      </div>
-      <div class="ui divided inbox selection list tab" data-tab="all">
-        <a class="item">
-          <div class="left floated ui star rating"><i class="link icon"></i></div>
-          <div class="right floated date">Sep 14, 2013</div>
-          <div class="description">There turns out there is only one article under all.</div>
-        </a>
-      </div>
-
-      <div class="ui divider"></div>
-
-      <div class="page">Showing <b>6</b> of 213</div>
-      <div class="ui pagination menu">
-        <a class="icon item"><i class="icon left arrow"></i></a>
-        <a class="active item">1</a>
-        <div class="disabled item">...</div>
-        <a class="item">10</a>
-        <a class="item">11</a>
-        <a class="item">12</a>
-        <a class="icon item"><i class="icon right arrow"></i></a>
-      </div>
-    </div>
-
-  </div>
-  <script src="../../assets/js/bandbrary.js"></script>
-</body>
-
-</html>
+      <?php endforeach; ?> -->
+    </body>
+    </html>
