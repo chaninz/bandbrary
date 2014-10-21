15b699b562bf408d3b47b109b361c3cea5817b4a
Chetniphat Varasai <titrez@outlook.com>
2014-10-22 02:37:51 +0700

    Update view job

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
-<footer>
+<!-- <footer>
 	<div class="footleft"></div>
 	<div class="footright"></div>
-</footer>
+</footer> -->
 <script src="<?php echo base_url().'assets/js/bandbrary.js'; ?>"></script>
 <script src="<?php echo base_url().'assets/js/jquery-ui.min.js'; ?>"></script>
 <script>
