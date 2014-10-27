832bd7311857bc233b7dc7b505cf531468187f70
Chetniphat Varasai <titrez@outlook.com>
2014-10-28 00:59:31 +0700

    Update view pm

--- application/views/private_message.php
+++ application/views/private_message.php
@@ -15,8 +15,17 @@
   <script src="feed.js"></script>
 
   <style>
+  body {
+    background: url('<?= base_url().'images/noise-black.png' ?>');
+  }
+  #pm-container {
+    border-left: 2px solid #000000; 
+    border-right: 2px solid #000000;  
+    -webkit-box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52);
+    box-shadow: 0 1px 1px rgba(0,0,0,.24),0 1px 5px rgba(0,0,0,.05); 
+  }
   #pm-inbox {
-    height: 600px;
+    height: 680px;
     overflow-y: scroll; 
     scrollbar-arrow-color:blue; 
     scrollbar-face-color: #e7e7e7; 
@@ -24,12 +33,17 @@
     scrollbar-darkshadow-color:#888888
   }
   #pm-msg {
-    height: 370px;
+    height: 380px;
     overflow-y: scroll; 
     scrollbar-arrow-color:blue; 
     scrollbar-face-color: #e7e7e7; 
     scrollbar-3dlight-color: #a0a0a0; 
     scrollbar-darkshadow-color:#888888
+  }
+  .pm-profile-pic {
+    height: 3em;
+    width: 3em;
+    margin-right: 10px!important;
   }
   .ui.segment {
     -webkit-box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.0); 
@@ -41,68 +55,78 @@
     padding-left: 1rem;
     padding-right: 1rem;
   }
+  .ui.feed .label img {
+    height: 3em;
+    width: 3em;
+    border-radius: 0em; 
+  }
+  .ui.feed .label + .content {
+    padding: 0em 1em 0em;
+  }
   </style>
 </head>
 <body id="feed">
   <?php $this->load->view('navigation'); ?>
 
-  <div class="container" style="border-left: 1px solid #D6D6D6; border-right: 1px solid #D6D6D6;">
+  <div id="pm-container" class="container">
     <div class="row">
-      <div class="col-xs-12" style="height: 100px; border-bottom: 1px solid #D6D6D6;">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-4" style="padding-top: 20px; padding-left: 0px; padding-right: 0px; border-right: 1px solid #D6D6D6;">
-        <h2 class="ui header" style="font-size: 1.3em; margin-left: 20px;">
+      <div class="col-xs-4" style="height: 100px; padding-top: 80px; border-right: 1px solid #D6D6D6; background-color: #FFFFFF;">
+        <h3 class="ui header" style="margin-left: 20px;">
           <i class="inbox icon"></i>
           กล่องข้อความ
         </h2>
+      </div>
+      <div class="col-xs-8" style="height: 100px; border-bottom: 1px solid #D6D6D6; background-color: #F7F6F6;"></div>
+    </div>
+    <div class="row">
+      <div class="col-xs-4" style="padding-top: 20px; padding-left: 0px; padding-right: 0px; border-right: 1px solid #D6D6D6; background-color: #FFFFFF;">
         <div id="pm-inbox" class="ui fluid divided inbox selection list active tab" data-tab="unread">
           <?php foreach($pm_users as $pm_user): ?>
           <a class="item" data-id="<?= $pm_user->id ?>" >
-            <div class="left floated ui star rating"><i class="icon"><?php if($pm_user->photo_url): ?><img class="icon" src="<?= base_url().'uploads/profile/'.$pm_user->photo_url ?>"><?php else: ?>
-              <img src="<?= base_url().'images/no_profile.jpg' ?>"><?php endif; ?></i></div>
-              <div class="right floated date"> <?= mdate("%d", strtotime($pm_user->timestamp)) ?> <?= mdate("%M", strtotime($pm_user->timestamp)) ?> <?= mdate("%Y", strtotime($pm_user->timestamp)) ?></div>           
-              <div class="description"><?= $pm_user->name ?> <?= $pm_user->surname ?> <?= $pm_user->text ?></div>
-            </a>
-          <?php endforeach; ?>
+           <?php if($pm_user->photo_url): ?><img class="icon" src="<?= base_url().'uploads/profile/'.$pm_user->photo_url ?>"><?php else: ?>
+           <img class="pm-profile-pic" src="<?= base_url().'images/no_profile.jpg' ?>"><?php endif; ?>
+           <div class="right floated date" style="margin-top: 4px;"> <?= mdate("%d", strtotime($pm_user->timestamp)) ?> <?= mdate("%M", strtotime($pm_user->timestamp)) ?> <?= mdate("%Y", strtotime($pm_user->timestamp)) ?></div>
+           <div class="description" style="margin-top: 4px;"><b><?= $pm_user->name ?> <?= $pm_user->surname ?></b></div>           
+           <?= $pm_user->text ?>    
+         </a>
+       <?php endforeach; ?>
+     </div>
+   </div>
+   <div class="col-xs-8" style="padding: 0px 0px 0px 0px; background-color: #FFFFFF;">
+     <div id="pm-msg" class="ui feed segment">
+      <?php foreach($chats as $chat): ?>
+      <div class="event">
+        <div class="label">
+         <?php if($chat->photo_url): ?>
+         <img src="<?= base_url().'uploads/profile/'.$chat->photo_url ?>"><?php else: ?>
+         <img src="<?= base_url().'images/no_profile.jpg' ?>"><?php endif; ?>
+         <div class="content">
+          <div class="date">
+           <?= mdate("%d", strtotime($chat->timestamp)) ?> 
+           <?= mdate("%M", strtotime($chat->timestamp)) ?> 
+           <?= mdate("%Y", strtotime($chat->timestamp)) ?>          
+         </div>
+         <div class="summary">
+           <a><?= $chat->from_user_name  ?> <?= $chat->from_user_surname  ?></a> 
+         </div>
+         <div class="extra text">
+          <?= $chat->text  ?>        
         </div>
       </div>
-      <div class="col-xs-8" style="padding: 0px 0px 0px 0px;">
-       <div id="pm-msg" class="ui feed segment">
-       <!--  <?php foreach($chats as $chat): ?>
-        <div class="event">
-          <div class="label">
-           <?php if($chat->photo_url): ?>
-           <img src="<?= base_url().'uploads/profile/'.$chat->photo_url ?>"><?php else: ?>
-           <img src="<?= base_url().'images/no_profile.jpg' ?>"><?php endif; ?>
-           <div class="content">
-            <div class="date">
-             <?= mdate("%d", strtotime($chat->timestamp)) ?> 
-             <?= mdate("%M", strtotime($chat->timestamp)) ?> 
-             <?= mdate("%Y", strtotime($chat->timestamp)) ?>          
-           </div>
-           <div class="summary">
-             <a><?= $chat->from_user_name  ?> <?= $chat->from_user_surname  ?></a> 
-           </div>
-           <div class="extra text">
-            <?= $chat->text  ?>        
-          </div>
-        </div>
-      </div>
-    <?php endforeach; ?> -->
-   
+    </div>
+  <?php endforeach; ?>
+
 </div>
 <form>
-<textarea name="message" class="ckeditor" style"border-top: 1px solid #b6b6b6;"></textarea>
-<input class="small ui button" style="margin-top: 5px; margin-left: 540px;" type="submit" value="send">
+  <div style="padding: 15px 15px 0px 15px; background-color: #F7F6F6;">
+    <textarea name="message" class="ckeditor" style"border-top: 1px solid #b6b6b6;"></textarea>
+  </div>
+  <div style="background-color: #F7F6F6;">
+    <input class="small ui button" style="margin-top: 10px; margin-bottom: 15px; margin-left: 540px;" type="submit" value="send">
+  </div>
 </form>
 </div>
 
-</div>
-<div class="row">
-  <div class="col-xs-12" style="height: 30px; border-top: 1px solid #D6D6D6;">
-  </div>
 </div>
 </div>
 
@@ -142,19 +166,20 @@
             
             html +=
             '<div class="event">'+
-              '<div class="label">'+image+
-                '<div class="content">'+
-                  '<div class="date">'+
-                    date.toDateString()+
-                  '</div>'+
-                  '<div class="summary">'+
-                    '<a>'+value.from_user_name + " " +value.from_user_surname  +'</a>'+
-                  '</div>'+
-                  '<div class="extra text">'+
-                    value.text+        
-                  '</div>'+
-                  '</div>'+
-              '</div>'+
+            '<div class="label">'+image+
+            '</div>'+
+            '<div class="content">'+
+            '<div class="date">'+
+            date.toDateString()+
+            '</div>'+
+            '<div class="summary">'+
+            '<a>'+'<b>'+value.from_user_name + " " +value.from_user_surname +'</b>'+'</a>'+
+            '</div>'+
+            '<div class="extra text">'+
+            value.text+        
+            '</div>'+
+            '</div>'+
+            '</div>'+
             '</div>';
           });
 
@@ -166,5 +191,8 @@
 })  
 
 </script>
+
+<?php $this->load->view('footer'); ?>
+
 </body>
 </html>
