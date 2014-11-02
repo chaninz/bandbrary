aab94f529d623e7fa0af868edd494391e7c0df28
PUNPUNsa <sa.punpun@gmail.com>
2014-11-02 14:11:58 +0700

    Update band status all

--- application/controllers/band/status.php
+++ application/controllers/band/status.php
@@ -11,16 +11,20 @@ class Status extends CI_Controller {
 	public function all($band_id) {
 		// Basic data for user profile page
 		$band_profile = $this->band_model->get($band_id);
+		$band_members = $this->join_band_model->get_current_member($band_id);
 
 		if ( ! empty($band_profile)) {
 			// Current user info
 			$current_user_id = $this->session->userdata('id');
 			// $is_follow_band = $this->follow_model->is_follow_band($current_user_id, $band_profile->id);
-			$user_status =  $this->join_band_model->get_user_status($current_user_id, $band_id);
+			//$user_status =  $this->join_band_model->get_user_status($current_user_id, $band_id);
+			
+			$data = array('statuss' => $this->status_model->get_by_band($band_id),
+				'band_profile' => $band_profile,
+				'band_members' => $band_members
+			);
+			$this->load->view('band/status_all', $data);
 
-			$all_status = $this->status_model->get_by_band($band_id);
-
-			print_r($all_status);
 			/*$data = array('band_profile' => $band_profile,
 				'is_follow_band' => $is_follow_band,
 				'user_status' => $user_status,
