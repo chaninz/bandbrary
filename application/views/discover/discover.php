f98254b291bc2a25e6ec2b824ebd6c8e2a5c0d32
Chetniphat Varasai <titrez@outlook.com>
2014-11-05 03:22:01 +0700

    Update discover

--- application/views/discover/discover.php
+++ application/views/discover/discover.php
@@ -58,12 +58,80 @@
 width: 50px;
 height: 50px;
 }
+i.icon.left {
+margin: 7em 0em 0em -1em;
+}
+i.icon.right {
+margin: 7em 0em 0em 1em;
+}
 	</style>
 </head>
 <body>
 	<?php $this->load->view('navigation'); ?>
 
 	<div class="container" style="padding-top: 140px;">
+		<div class="row">
+
+			<div class="col-xs-12">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="width: 920px; height: 500px; margin-left: 30px;">
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class=""></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+        <li data-target="#carousel-example-generic" data-slide-to="3" class=""></li>
+        <li data-target="#carousel-example-generic" data-slide-to="4" class=""></li>
+        <li data-target="#carousel-example-generic" data-slide-to="5" class=""></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item">
+          <img src="<?= base_url('images/maroon5.jpg') ?>" alt="">
+          <div class="carousel-caption">
+    		<h3>New Album</h3>
+  		  </div>
+        </div>
+        <div class="item active">
+        	<img src="<?= base_url('images/linkinpark.jpg') ?>" alt="">
+        	<div class="carousel-caption">
+    		<h3>New Album</h3>
+  		  </div>
+        </div>
+        <div class="item">
+			<img src="<?= base_url('images/taylorswift.jpg') ?>" alt="">
+			<div class="carousel-caption">
+    		<h3>New Album</h3>
+  		  </div>
+        </div>
+        <div class="item">
+			<img src="<?= base_url('images/taylorswift.jpg') ?>" alt="">
+			<div class="carousel-caption">
+    		<h3>New Album</h3>
+  		  </div>
+        </div>
+        <div class="item">
+			<img src="<?= base_url('images/taylorswift.jpg') ?>" alt="">
+			<div class="carousel-caption">
+    		<h3>New Album</h3>
+  		  </div>
+        </div>
+        <div class="item">
+			<img src="<?= base_url('images/taylorswift.jpg') ?>" alt="">
+			<div class="carousel-caption">
+    		<h3>New Album</h3>
+  		  </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span><i class="left arrow icon" style="font-size: 1.7em;"></i></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span><i class="right arrow icon" style="font-size: 1.7em;"></i></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+
+		</div>
 		<h3 class="ui header" style="margin-left: 30px;">แนะนำเพลง</h3>
 		<div class="row">
 			<div class="col-xs-12"> 
