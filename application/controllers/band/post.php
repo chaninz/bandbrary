6a6a2d87a1e626923155a55d59c919c9801b0ee6
Chanin N <chaninz@live.com>
2014-11-05 15:16:33 +0700

    Edit post has complete

--- application/controllers/band/post.php
+++ application/controllers/band/post.php
@@ -10,11 +10,12 @@ class Post extends CI_Controller {
 		$this->load->model('follow_model');
 		$this->load->model('join_band_model');
 		$this->load->model('skill_model');
-		// Page model
+		// Page import
 		$this->load->model('post_model');
 		$this->load->model('postcomment_model');
 		$this->load->library('upload');
 		$this->load->library('image_lib');
+		$this->lang->load('upload', 'thai');
 	}
 
 
@@ -54,16 +55,18 @@ class Post extends CI_Controller {
 		$post = $this->input->post('post');
 
 		if ( ! empty($band_id) && ! empty($topic) && ! empty($post)) {
+			// If data is sent from form
 			$post_image_name = $this->input->post('post-image-name');
 			$data = array('band_id' => $band_id,
 				'topic' => $topic,
 				'post' => $post);
+			$is_uploaded = 1;
 
 			if ( ! empty($post_image_name)) {
 				$photo_path = realpath('uploads/post/band');
 				$photo_name = $band_id.'-post-'.time();
 
-				// Upload cover photo to server
+				// Upload photo to server
 				$config = array('allowed_types' => 'jpg|jpeg',
 					'max_size' => 1024,
 					'overwrite' => TRUE,
@@ -79,47 +82,149 @@ class Post extends CI_Controller {
 					$data['image_url'] = $image_data['file_name'];
 
 					// Resize to 600px
-					$config = array(
-						'source_image' => $image_data['full_path'],
+					$config = array('source_image' => $image_data['full_path'],
 						'new_image' => $photo_path,
-						'maintain_ratio' => TRUE,
+						'master_dim' => 'width',
+						'height' => 1,
 						'width' => 600);
 					$this->image_lib->initialize($config);
 					$this->image_lib->resize();
 				}
 			}
 
-			$this->post_model->add($data);
-			redirect(base_url('band/'.$band_id.'/post'));
+			if ($is_uploaded == 0) {
+				// Basic data for user profile page
+				$band_profile = $this->band_model->get($band_id);
+				$status = $this->status_model->get_last_by_band($band_id);
+				$band_members = $this->join_band_model->get_current_member($band_id);
+				// Current user info
+				$current_user_id = $this->session->userdata('id');
+				$is_follow_band = $this->follow_model->is_follow_band($current_user_id, $band_profile->id);
+				$user_status =  $this->join_band_model->get_user_status($current_user_id, $band_id);
+				$current_user_skills = $this->skill_model->get_by_user($current_user_id);
+
+				$display = array('band_profile' => $band_profile,
+					'status' => $status,
+					'band_members' => $band_members,
+					'is_follow_band' => $is_follow_band,
+					'user_status' => $user_status,
+					'current_user_skills' => $current_user_skills,
+					'msg_image' => $msg_image,
+					'topic' => $topic,
+					'post' => $post);
+				
+				$this->load->view('band/add_post', $display);
+			} else {
+				$this->post_model->add($data);
+				redirect(base_url('band/'.$band_id.'/post'));
+			}
 		} else {
-			show_404();
+			// View page
+			// Basic data for user profile page
+			$band_profile = $this->band_model->get($band_id);
+
+			if ( ! empty($band_profile)) {
+				// Basic data for user profile page
+				$status = $this->status_model->get_last_by_band($band_id);
+				$band_members = $this->join_band_model->get_current_member($band_id);
+				// Current user info
+				$current_user_id = $this->session->userdata('id');
+				$is_follow_band = $this->follow_model->is_follow_band($current_user_id, $band_profile->id);
+				$user_status =  $this->join_band_model->get_user_status($current_user_id, $band_id);
+				$current_user_skills = $this->skill_model->get_by_user($current_user_id);
+				// Page data
+				
+				$display = array('band_profile' => $band_profile,
+					'status' => $status,
+					'band_members' => $band_members,
+					'is_follow_band' => $is_follow_band,
+					'user_status' => $user_status,
+					'current_user_skills' => $current_user_skills);
+
+				$this->load->view('band/add_post', $display);
+			}
 		}
-
-
 	}
 
 	public function edit($post_id) {
-		if ($this->input->post()) {
-			$post = array('post' => $this->input->post('post'),
-				'image_url' => $this->input->post('imageurl')
-			);
-			$this->post_model->editPost($post);
+		$post = $this->post_model->get($post_id);
+		$band_id = $this->session->userdata('band_id');
+
+		if ($post->band_id == $band_id) {
+			// Check if is the band master
+			$topic = $this->input->post('topic');
+			$content = $this->input->post('content');
+			$is_image_upload = $this->input->post('post-image-upload');
+
+			if ( ! empty($topic) && ! empty($content)) {
+				// Edit data
+				$new_data = array('topic' => $topic,
+					'post' => $content);
+				$msg_image = NULL;
+
+				if ($is_image_upload == 1) {
+					// Delete profile photo
+					$new_data['image_url'] = NULL;
+				} else if ($is_image_upload == 2) {
+					$photo_path = realpath('uploads/post/band');
+					$photo_name = $band_id.'-post-'.time();
+
+					// Upload photo to server
+					$config = array('allowed_types' => 'jpg|jpeg',
+						'max_size' => 1024,
+						'overwrite' => TRUE,
+						'file_name' => $photo_name,
+						'upload_path' => $photo_path.'/original');
+					$this->upload->initialize($config);
+					$result = $this->upload->do_upload('post-image');
+					$msg_image = $this->upload->display_errors();
+					$image_data = $this->upload->data();
+
+					if ( ! empty($result)) {
+						// If upload complete
+						$new_data['image_url'] = $image_data['file_name'];
+
+						// Resize to 600px
+						$config = array('source_image' => $image_data['full_path'],
+							'new_image' => $photo_path,
+							'master_dim' => 'width',
+							'height' => 1,
+							'width' => 600);
+						$this->image_lib->initialize($config);
+						$this->image_lib->resize();
+					}
+				}
+
+				// Update new data to database
+				$this->post_model->edit($post_id, $new_data);
+
+				// Retrieve data for refresh page
+				$post = $this->post_model->get($post_id);
+				$msg = array('type' => 3, 
+					'header' => '',
+					'text' => 'บันทึกข้อมูลเรียบร้อย');
+				$display = array('post' => $post,
+					'msg_image' => $msg_image,
+					'msg' => $msg);
+				$this->load->view('band/edit_post', $display);
+			} else {
+				// If not send data from form
+				$display = array('post' => $post);
+				$this->load->view('band/edit_post', $display);
+			}
 		} else {
-			$data = array(
-			'post' => $this->post_model->getPost($post_id)
-			);
-			$this->load->view('band/editPost',$data);
+			show_404();
 		}
 	}
 
-	public function delete($post_id){
-			$band_id = $this->session->userdata('band_id');
-			$this->post_model->deletePost($post_id);
-			redirect('/band/'.$band_id.'/post');
+	public function delete($post_id) {
+		$band_id = $this->session->userdata('band_id');
+		$this->post_model->deletePost($post_id);
+		redirect('/band/'.$band_id.'/post');
 
 	}
 
-	public function view($post_id){
+	public function view($post_id) {
 		// Basic data for user profile page
 		$post = $this->post_model->get($post_id);
 
--- application/models/post_model.php
+++ application/models/post_model.php
@@ -6,30 +6,6 @@ class Post_model extends CI_Model {
 		$this->db->insert('Band_Posts', $data);	
 	}
 
-	// function get_by_user($id, $user_type) {
-	// 	if ($user_type == 1) {
-	// 		//$query = $this->db->get_where('User_Events', array('user_id' => $id));
-	// 	} elseif ($user_type == 2) {
-	// 		$query = $this->db->get_where('Band_Posts', array('band_id' => $id));
-	// 	}
-	// 	$result = $query->result();
-
-	// 	return $result;
-	// }
-
-	// function get_band_post($band_id) {
-	// 	return $this->get_by_user($band_id, 2);
-	// }
-
-	function getPost($post_id){
-		$this->db->select('*');
-		$this->db->from('Band_Posts');
-		$this->db->where('id',$post_id);
-
-		$query = $this->db->get();
-		return $query->row();
-	}
-
 	function get($post_id) {
 		$query = $this->db->get_where('Band_Posts', array('id' => $post_id));
 		$result = $query->row();
@@ -49,6 +25,13 @@ class Post_model extends CI_Model {
 		return $result;
 	}
 
+	function edit($post_id, $data) {
+		$this->db->where('id', $post_id);
+		$this->db->update('Band_Posts', $data);
+	}
+
+//----------------------
+
 	function editPost($data){
 		$id = $this->session->userdata('id');
 		$this->db->where('id',$id);
@@ -59,6 +42,30 @@ class Post_model extends CI_Model {
 		$this->db->where('id', $post_id);
 		$this->db->delete('Band_Posts');
 	}
+
+	function getPost($post_id){
+		$this->db->select('*');
+		$this->db->from('Band_Posts');
+		$this->db->where('id',$post_id);
+
+		$query = $this->db->get();
+		return $query->row();
+	}
+
+	// function get_by_user($id, $user_type) {
+	// 	if ($user_type == 1) {
+	// 		//$query = $this->db->get_where('User_Events', array('user_id' => $id));
+	// 	} elseif ($user_type == 2) {
+	// 		$query = $this->db->get_where('Band_Posts', array('band_id' => $id));
+	// 	}
+	// 	$result = $query->result();
+
+	// 	return $result;
+	// }
+
+	// function get_band_post($band_id) {
+	// 	return $this->get_by_user($band_id, 2);
+	// }
 
 }
 
